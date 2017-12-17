5bfca61045e37f97107d17b618116ae0c0ea240d
Zex <913448071@qq.com>
2017-12-17 11:12:13 +0800

    补充上传亚马逊登陆依赖库

---
+++ linkuslight/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/Build/Intermediates.noindex/LoginWithAmazonSample.build/Debug-iphoneos/LoginWithAmazonSample.build/Objects-normal/arm64/AMZNAppDelegate.d
@@ -0,0 +1,17 @@
+dependencies: \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/LoginWithAmazonSample-Prefix.pch \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/AMZNAppDelegate.m \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/LoginWithAmazon.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AIMobileLib.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AIError.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AIAuthenticationDelegate.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNScope.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNProfileScope.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNScopeFactory.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNUser.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNAuthorizeRequest.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNAuthorizeResult.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNAuthorizationManager.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/../Library/LoginWithAmazon.framework/Headers/AMZNLWASDKInfo.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/AMZNAppDelegate.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/AMZNLoginController.h
---
+++ linkuslight/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/Build/Intermediates.noindex/LoginWithAmazonSample.build/Debug-iphoneos/LoginWithAmazonSample.build/Objects-normal/arm64/main.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/LoginWithAmazonSample-Prefix.pch \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/main.m \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/AMZNAppDelegate.h \
+  /Users/zex/Documents/Workspace/AmazonAWS/Amazon-iOS-SDKs/LoginWithAmazon/examples/SampleLoginWithAmazonApp/LoginWithAmazonSample/LoginWithAmazonSample/AMZNLoginController.h
